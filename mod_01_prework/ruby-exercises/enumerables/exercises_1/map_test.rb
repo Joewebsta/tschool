13b78c5afc9d2fccf9ac429518b5041db3c84c68
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-01 10:41:44 -0400

    Pass tests for enumerables/map_test.rb

--- mod_01_prework/ruby-exercises/enumerables/exercises_1/map_test.rb
+++ mod_01_prework/ruby-exercises/enumerables/exercises_1/map_test.rb
@@ -3,64 +3,53 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class MapTest < Minitest::Test
-
   def test_capitalize
-    names = ["alice", "bob", "charlie"]
-    capitalized_names = names.map do |name|
-      name.capitalize
-    end
-    assert_equal ["Alice", "Bob", "Charlie"], capitalized_names
+    names = %w[alice bob charlie]
+    capitalized_names = names.map(&:capitalize)
+    assert_equal %w[Alice Bob Charlie], capitalized_names
   end
 
   def test_doubles
-    skip
     numbers = [1, 2, 3, 4, 5]
     doubles = numbers.map do |number|
-      # Your code goes here
+      number * 2
     end
     assert_equal [2, 4, 6, 8, 10], doubles
   end
 
   def test_squares
-    skip
     numbers = [1, 2, 3, 4, 5]
-    # Your code goes here
+    squares = numbers.map { |num| num.pow(2) }
     assert_equal [1, 4, 9, 16, 25], squares
   end
 
   def test_lengths
-    skip
-    names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
+    names = %w[alice bob charlie david eve]
+    lengths = names.map(&:length)
     assert_equal [5, 3, 7, 5, 3], lengths
   end
 
   def test_normalize_zip_codes
-    skip
-    numbers = [234, 10, 9119, 38881]
-    # Your code goes here
-    assert_equal ["00234", "00010", "09119", "38881"], zip_codes
+    numbers = [234, 10, 9119, 38_881]
+    zip_codes = numbers.map { |num| num.to_s.rjust(5, '0') }
+    assert_equal %w[00234 00010 09119 38881], zip_codes
   end
 
   def test_backwards
-    skip
-    names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
-    assert_equal ["ecila", "bob", "eilrahc", "divad", "eve"], backwards
+    names = %w[alice bob charlie david eve]
+    backwards = names.map { |name| name.reverse }
+    assert_equal %w[ecila bob eilrahc divad eve], backwards
   end
 
   def test_words_with_no_vowels
-    skip
-    words = ["green", "sheep", "travel", "least", "boat"]
-    # Your code goes here
-    assert_equal ["grn", "shp", "trvl", "lst", "bt"], without_vowels
+    words = %w[green sheep travel least boat]
+    without_vowels = words.map { |word| word.delete('aeiou') }
+    assert_equal %w[grn shp trvl lst bt], without_vowels
   end
 
   def test_trim_last_letter
-    skip
-    animals = ["dog", "cat", "mouse", "frog", "platypus"]
-    # Your code goes here
-    assert_equal ["do", "ca", "mous", "fro", "platypu"], trimmed
+    animals = %w[dog cat mouse frog platypus]
+    trimmed = animals.map { |animal| animal.chop }
+    assert_equal %w[do ca mous fro platypu], trimmed
   end
-
 end
