d782d9bc4b0c4c61046c1b07dc0fa1226a20443f
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-01 11:26:27 -0400

    Pass tests for enumerables/select_test.rb

--- mod_01_prework/ruby-exercises/archive/enum_exercises/select_test.rb
+++ mod_01_prework/ruby-exercises/archive/enum_exercises/select_test.rb
@@ -3,7 +3,6 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class SelectTest < Minitest::Test
-
   def test_pick_even_numbers
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     evens = numbers.select do |number|
@@ -15,71 +14,62 @@ class SelectTest < Minitest::Test
   def test_pick_odd_numbers
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     odds = numbers.select do |number|
-      # Your code goes here
+      number.odd?
     end
     assert_equal [1, 3, 5, 7, 9], odds
   end
 
   def test_pick_words_with_three_letters
-    skip
-    words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
-    # Your code goes here
-    assert_equal ["bad", "cat", "dog", "red"], selected
+    words = %w[pill bad finger cat blue dog table red]
+    selected = words.select { |word| word.length == 3 }
+    assert_equal %w[bad cat dog red], selected
   end
 
   def test_pick_words_with_more_than_three_letters
-    skip
-    words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
-    # Your code goes here
-    assert_equal ["pill", "finger", "blue", "table"], selected
+    words = %w[pill bad finger cat blue dog table red]
+    selected = words.select { |word| word.length > 3 }
+    assert_equal %w[pill finger blue table], selected
   end
 
   def test_pick_words_ending_in_e
-    skip
-    words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
-    # Your code goes here
-    assert_equal ["are", "strike", "piece", "warble", "pipe"], selected
+    words = %w[are you strike thinking belt piece warble sing pipe]
+    selected = words.select { |word| word.end_with?('e') }
+    assert_equal %w[are strike piece warble pipe], selected
   end
 
   def test_pick_words_ending_in_ing
-    skip
-    words = ["bring", "finger", "drought", "singing", "bingo", "purposeful"]
-    # Your code goes here
-    assert_equal ["bring", "singing"], selected
+    words = %w[bring finger drought singing bingo purposeful]
+    selected = words.select { |word| word.end_with?('ing') }
+    assert_equal %w[bring singing], selected
   end
 
   def test_pick_words_containing_e
-    skip
-    words = ["four", "red", "five", "blue", "pizza", "purple"]
-    # Your code goes here
-    assert_equal ["red", "five", "blue", "purple"], selected
+    words = %w[four red five blue pizza purple]
+    selected = words.select { |word| word.include?('e') }
+    assert_equal %w[red five blue purple], selected
   end
 
   def test_pick_dinosaurs
-    skip
-    animals = ["tyrannosaurus", "narwhal", "eel", "achillesaurus", "qingxiusaurus"]
-    # Your code goes here
-    assert_equal ["tyrannosaurus", "achillesaurus", "qingxiusaurus"], dinosaurs
+    animals = %w[tyrannosaurus narwhal eel achillesaurus qingxiusaurus]
+    dinosaurs = animals.select { |animal| animal.end_with?('saurus') }
+    assert_equal %w[tyrannosaurus achillesaurus qingxiusaurus], dinosaurs
   end
 
   def test_pick_floats
-    skip
     numbers = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
-    # Your code goes here
+    floats = numbers.select { |num| num.instance_of?(Float) }
     assert_equal [1.4, 3.5, 4.9, 9.1, 8.0], floats
   end
 
   def test_pick_arrays
-    skip
-    elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"]
-    # Your code goes here
-    assert_equal [["dog"], [56, 3, 8]], arrays
+    elements = ['CAT', ['dog'], 23, [56, 3, 8], 'AIMLESS', 43, 'butter']
+    arrays = elements.select { |el| el.instance_of?(Array) }
+    assert_equal [['dog'], [56, 3, 8]], arrays
   end
 
   def test_pick_hashes
-    skip
-    elements = ["cat", {:dog=>"fido"}, 23, {:stuff=>"things"}, "aimless", 43]
-    # Your code goes here
-    assert_equal [{:dog=>"fido"}, {:stuff=>"things"}], hashes
+    elements = ['cat', { dog: 'fido' }, 23, { stuff: 'things' }, 'aimless', 43]
+    hashes = elements.select { |el| el.instance_of?(Hash) }
+    assert_equal [{ dog: 'fido' }, { stuff: 'things' }], hashes
   end
 end
