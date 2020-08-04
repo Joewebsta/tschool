6e9d69fe96af2349abb389a573028415e23fbb3c
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-04 15:02:18 -0400

    Write first test for Turn#type (:basic) and corresponding class code

--- mod_01/projects/war_or_peace/lib/turn.rb
+++ mod_01/projects/war_or_peace/lib/turn.rb
@@ -1,2 +1,17 @@
+require './lib/deck'
+
 class Turn
+  attr_reader :player1, :player2
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def type
+    player1_card = player1.deck.rank_of_card_at(0)
+    player2_card = player2.deck.rank_of_card_at(0)
+
+    :basic if player1_card != player2_card
+  end
 end
