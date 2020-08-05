a852b157ee0bb52144c5fb3db07efc72637d3285
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-05 15:43:17 -0400

    Move player card ranks into Turn#init and write code for Turn#winner when type is basic

--- mod_01/projects/war_or_peace/lib/turn.rb
+++ mod_01/projects/war_or_peace/lib/turn.rb
@@ -6,16 +6,20 @@ class Turn
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+
+    @p1_card1_rank = @player1.deck.rank_of_card_at(0)
+    @p1_card3_rank = @player1.deck.rank_of_card_at(2)
+    @p2_card1_rank = @player2.deck.rank_of_card_at(0)
+    @p2_card3_rank = @player2.deck.rank_of_card_at(2)
   end
 
   def type
-    p1_card1_rank = player1.deck.rank_of_card_at(0)
-    p1_card3_rank = player1.deck.rank_of_card_at(2)
-    p2_card1_rank = player2.deck.rank_of_card_at(0)
-    p2_card3_rank = player2.deck.rank_of_card_at(2)
+    return :basic unless @p1_card1_rank == @p2_card1_rank
+    return :mutually_assured_destruction if @p1_card1_rank == @p2_card1_rank && @p1_card3_rank == @p2_card3_rank
+    return :war if @p1_card1_rank == @p2_card1_rank
+  end
 
-    return :basic unless p1_card1_rank == p2_card1_rank
-    return :mutually_assured_destruction if p1_card1_rank == p2_card1_rank && p1_card3_rank == p2_card3_rank
-    return :war if p1_card1_rank == p2_card1_rank
+  def winner
+    return @p1_card1_rank > @p2_card1_rank ? player1 : player2 if type == :basic
   end
 end
