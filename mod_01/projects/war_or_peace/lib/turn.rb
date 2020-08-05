23d4ec39a512ad4bec7fbadfce711ee628c1b76f
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-05 16:10:26 -0400

    Write code to pass tests for Turn#winner when type is :war

--- mod_01/projects/war_or_peace/lib/turn.rb
+++ mod_01/projects/war_or_peace/lib/turn.rb
@@ -21,5 +21,6 @@ class Turn
 
   def winner
     return @p1_card1_rank > @p2_card1_rank ? player1 : player2 if type == :basic
+    return @p1_card3_rank > @p2_card3_rank ? player1 : player2 if type == :war
   end
 end
