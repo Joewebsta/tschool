0ce1c1d1e457da22b08dfc5bd7b7706b238fd995
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-06 19:41:09 -0400

    Identify and fix hobbit errors and test failures

--- mod_02/erroneous_creatures-master/lib/hobbit.rb
+++ mod_02/erroneous_creatures-master/lib/hobbit.rb
@@ -1,9 +1,8 @@
 class Hobbit
-
   attr_reader :name, :disposition
   attr_accessor :age, :playable, :tiredness
 
-  def initialize (name, disposition="homebody")
+  def initialize(name, disposition = 'homebody')
     @name = name
     @disposition = disposition
     @age = 0
@@ -15,7 +14,7 @@ class Hobbit
   end
 
   def adult?
-    @agee >= 32
+    @age > 32
   end
 
   def play
