c8f5b00e12a51522ae465b90c813f42deaa059d9
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-06 15:34:59 -0400

    Update terminal output for each turn

--- mod_01/projects/war_or_peace/lib/game.rb
+++ mod_01/projects/war_or_peace/lib/game.rb
@@ -7,11 +7,22 @@ class Game
   end
 
   def start
-    5.times do
+    (1..1_000_000).each do |turn_num|
+      puts "Player 1 cards: #{@player1.deck.cards}"
+      puts "Player 2 cards: #{@player2.deck.cards}"
+      puts
+
       turn = Turn.new(@player1, @player2)
       turn.pile_cards
       turn.award_spoils
-      puts turn.winner.name
+      type = turn.type
+
+      output = "Turn #{turn_num}: "
+      output += "#{turn.winner.name} won 2 cards" if type == :basic
+      output += "WAR - #{turn.winner.name} won 6 cards" if type == :war
+      output += '*mutually assured destruction* 6 cards removed from play' if type == :mutually_assured_destruction
+      puts output
+      puts
     end
   end
 end
