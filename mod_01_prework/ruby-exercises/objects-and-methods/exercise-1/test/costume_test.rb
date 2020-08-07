1f32ffaac361c5b34d9bf23ca7f5a6bf0daca321
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-07 19:14:35 -0400

    Pass Costume tests

--- mod_01_prework/ruby-exercises/objects-and-methods/exercise-1/test/costume_test.rb
+++ mod_01_prework/ruby-exercises/objects-and-methods/exercise-1/test/costume_test.rb
@@ -5,14 +5,12 @@ require_relative '../lib/costume'
 
 class CostumeTest < Minitest::Test
   def test_costume_style
-    costume = Costume.new("Batman")
-    assert_equal "Batman", costume.style
+    costume = Costume.new('Batman')
+    assert_equal 'Batman', costume.style
   end
 
   def test_other_style_of_costume
-    skip
-    costume = Costume.new("Princess")
-    assert_equal "Princess", costume.style
+    costume = Costume.new('Princess')
+    assert_equal 'Princess', costume.style
   end
 end
-
