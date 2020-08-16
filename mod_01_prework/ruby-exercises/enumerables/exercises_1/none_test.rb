f7f1c47f99c8154f86137f3294baaf85e4dc0fd6
Joe Webster <joseph.scott.webster@gmail.com>
2020-08-16 10:50:04 -0400

    Pass tests for none_test.rb

--- mod_01_prework/ruby-exercises/enumerables/exercises_1/none_test.rb
+++ mod_01_prework/ruby-exercises/enumerables/exercises_1/none_test.rb
@@ -3,49 +3,41 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class NoneTest < Minitest::Test
-
   def test_none_are_broken
-    things = ["functional", "working", "works", "fixed", "good"]
+    things = %w[functional working works fixed good]
     none_broken = things.none? do |thing|
-      thing == "broken"
+      thing == 'broken'
     end
     assert none_broken
   end
 
   def test_double_negative
     numbers = [9, 3, 3, 7, 6, -5, 1]
-    not_none_negative = numbers.none? do |number|
-      # Your code goes here
-    end
+    not_none_negative = numbers.none?(&:negative?)
     refute not_none_negative
   end
 
   def test_none_are_negative
-    skip
     numbers = [9, 3, 1, 8, 3, 3, 5]
-    # Your code goes here
+    none_negative = numbers.none?(&:negative?)
     assert none_negative
   end
 
   def test_none_shall_pass
-    skip
-    critters = ["elf", "hobbit", "dwarf", "wizard", "human"]
-    # Your code goes here
+    critters = %w[elf hobbit dwarf wizard human]
+    none_shall_pass = critters.none? { |critter| critter == 'orc' }
     assert none_shall_pass
   end
 
   def test_one_or_more_shall_pass
-    skip
-    phrases = ["go", "left", "can cross", "shall pass", "must stay", "tarried"]
-    # Your code goes here
+    phrases = ['go', 'left', 'can cross', 'shall pass', 'must stay', 'tarried']
+    none_shall_pass = phrases.none? { |phrase| phrase == 'go' }
     refute none_shall_pass
   end
 
   def test_none_even
-    skip
     numbers = [3, 9, 15, 21, 19]
-    # Your code goes here
+    none_even = numbers.none?(&:even?)
     assert none_even
   end
-
 end
